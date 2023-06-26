46a04fcd6dc61245f71e39632907dda5dd611dfe
afisman <afisman@gmail.com>
2023-06-26 11:28:57 +0200

    updated providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiConfig, createConfig, configureChains, } from 'wagmi'
+import { mainnet, goerli, sepolia } from 'wagmi/chains'
+
+import { publicProvider } from 'wagmi/providers/public'
+
+const { publicClient, webSocketPublicClient, chains } = configureChains(
+  [mainnet, goerli, sepolia],
+  [publicProvider()],
+)
+
+const config = createConfig({
+  publicClient,
+  webSocketPublicClient,
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={config}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>,
 )
